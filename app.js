cb61ed2f2c5a6d71d83bfdb5e34aaf3d83644120
Bhavesh Lohana <53433312+bhaveshlohana@users.noreply.github.com>
2020-07-10 19:46:10 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const hex=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn=document.getElementById("btn");
+const color=document.querySelector(".color");
+
+btn.addEventListener("click", function() {
+	let hexColor="#"+hex[randomHex()]+hex[randomHex()]+hex[randomHex()]+hex[randomHex()]+hex[randomHex()]+hex[randomHex()];
+	document.body.style.backgroundColor=hexColor;
+	color.textContent=hexColor;
+
+});
+
+function randomHex() {
+	return Math.floor(Math.random()*hex.length);
+}
